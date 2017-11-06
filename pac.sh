f5c469003eb49c6c72aeca396a5a732f6cb8e0c5
Jenkins <hshengyan@ceibs.edu>
2017-11-06 21:02:29 +0800

    修改pac.sh脚本

--- pac.sh
+++ pac.sh
@@ -5,6 +5,6 @@ echo "hosts = [" > pac.toml
 grep '": 1' abc.pac  | awk -F\" '{print " \""$2"\","}' >> pac.toml
 
 #自定义的域名加到这里
-echo " \"aisex.com\"," >> pac.toml
+echo " \"google.com\"," >> pac.toml
 
 echo "]" >> pac.toml
