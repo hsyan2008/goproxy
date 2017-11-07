4a05ef5ccca5cd638407d682d15064dbbf03051d
Jenkins <hshengyan@ceibs.edu>
2017-11-07 09:25:14 +0800

    修改pac.sh脚本在一个错误

--- pac.sh
+++ pac.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 rm -rf abc.pac
 aria2c -q https://pac.itzmx.com/abc.pac
-echo "hosts = [" > pac.toml
+echo "prehosts = [" > pac.toml
 grep '": 1' abc.pac  | awk -F\" '{print " \""$2"\","}' >> pac.toml
 
 #自定义的域名加到这里
